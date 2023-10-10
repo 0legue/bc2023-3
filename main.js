c51f417b9c14ddfaa433c3322926f301b4c66333
Oleg Homych <138622614+0legue@users.noreply.github.com>
2023-10-10 14:57:09 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,19 +1,34 @@
-const fs = require("fs");
+const fs = require('fs');
 
-fs.readFile("data.json", (err, data) => {
-    if (err === null) {
-        let jsonData = JSON.parse(data);
-        const filteredData = jsonData.filter(entry => entry.parent === "BS3_BanksLiab");
-        const outputText = filteredData.map(entry => `${entry.txten}:${entry.value}`).join('\n');
+fs.readFile('data.json', 'utf8', (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
 
-        fs.writeFile('output.txt', outputText, (err) => {
-            if (err === null) {
-                console.log("Saved!");
-            } else {
-                console.log(err);
-            }
-        });
-    } else {
-        console.log(err);
+  try {
+    const jsonData = JSON.parse(data);
+    let minAsset = null;
+    for (const asset of jsonData) {
+      if (!minAsset || asset.value < minAsset.value) {
+        minAsset = asset;
+      }
     }
+
+    if (minAsset) {
+      const resultString = `${minAsset.txt}:${minAsset.value}`;
+      fs.writeFile('output.txt', resultString, (err) => {
+        if(err){
+          console.error(err);
+        } else {
+          console.log(`Дані записані у файл output.txt`);
+        }
+      });
+
+    } else {
+      console.log(`Немає даних для обробки.`);
+    }
+  } catch (parseError) {
+    console.error(`Помилка розбору JSON: ${parseError.message}`);
+  }
 });
