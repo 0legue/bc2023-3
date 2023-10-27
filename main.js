8129e343f2e6d807a6928bdd72336508b334500b
0legue <peentagrammee@gmail.com>
2023-10-27 10:37:39 +0300

    dekstop

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
 const fs = require('fs');
+const path = require('path');
+
+const desktopPath = path.join(require('os').homedir(), 'Desktop'); // Отримуємо шлях до робочого столу
+
+const outputFile = path.join(desktopPath, 'output.txt'); // Формуємо шлях до файлу на робочому столі
 
 fs.readFile('data.json', 'utf8', (err, data) => {
   if (err) {
@@ -17,14 +22,13 @@ fs.readFile('data.json', 'utf8', (err, data) => {
 
     if (minAsset) {
       const resultString = `${minAsset.txt}:${minAsset.value}`;
-      fs.writeFile('output.txt', resultString, (err) => {
-        if(err){
+      fs.writeFile(outputFile, resultString, (err) => {
+        if (err) {
           console.error(err);
         } else {
-          console.log(`Дані записані у файл output.txt`);
+          console.log(`Дані записані у файл ${outputFile}`);
         }
       });
-
     } else {
       console.log(`Немає даних для обробки.`);
     }
